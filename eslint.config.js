893d236f2d1f4521b13b5c50fbb9620cd7f05bd8
Kai Nguyen <hungnh1704@gmail.com>
2024-11-18 00:34:40 +0700

    feat(config): add config for nestjs to ignore client

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,7 @@ export default [
       node: true,
       jest: true,
     },
-    ignorePatterns: ['.eslintrc.js'],
+    ignorePatterns: ['.eslintrc.js', 'client'],
     rules: {
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
